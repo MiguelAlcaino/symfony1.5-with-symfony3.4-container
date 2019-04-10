c5192a5d0102b63445e5c3f6e12d15b899a12229
Miguel Alcaino <miguel.alcaino@ogb.cl>
2019-04-10 18:35:01 +0200

    first commit

---
+++ lib/form/BaseForm.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Base project form.
+ * 
+ * @package    jobbet2
+ * @subpackage form
+ * @author     Your name here 
+ * @version    SVN: $Id$
+ */
+class BaseForm extends sfFormSymfony
+{
+}
