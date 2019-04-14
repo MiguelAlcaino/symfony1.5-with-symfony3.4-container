636756846775cd166585ab6fcfbec9612aee2027
Miguel Alcaino <miguel.alcaino@ogb.cl>
2019-04-14 22:26:46 +0200

    job module added

---
+++ apps/amboss/modules/job/actions/actions.class.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * job actions.
+ *
+ * @package    jobbet2
+ * @subpackage job
+ * @author     Your name here
+ * @version    SVN: $Id$
+ */
+class jobActions extends sfActions
+{
+    /**
+     * Executes index action
+     *
+     * @param sfRequest $request A request object
+     */
+    public function executeIndex(sfWebRequest $request)
+    {
+        $logger = $this->getService(\MiguelAlcaino\Service\Logger::class);
+
+        $this->getResponse()->setContent(json_encode(['hola' => 'pianola', 'logger' => $logger->log('HOLANDAQUETALCA')]));
+
+        return sfView::NONE;
+    }
+}
